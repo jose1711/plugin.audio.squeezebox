b3fbe4e60564c9e2e52025289faf30a964230eb2
foka <mokrafoka@gmail.com>
2023-12-08 14:30:56 +0100

    Fixed syntax issues for Python >3.5

--- resources/lib/lmsserver.py
+++ resources/lib/lmsserver.py
@@ -306,13 +306,13 @@ class LMSDiscovery(object):
         sock.settimeout(lms_timeout)
         sock.bind(('', 0))
         try:
-            sock.sendto(lms_msg, (lms_ip, lms_port))
+            sock.sendto(lms_msg.encode('utf-8'), (lms_ip, lms_port))
             while True:
                 try:
                     data, server = sock.recvfrom(1024)
                     host, _ = server
                     if data.startswith(b'E'):
-                        port = data.split("\x04")[1]
+                        port = data.split(b'\x04')[1]
                         entries.append({'port': int(port),
                                         'data': data,
                                         'from': server,
